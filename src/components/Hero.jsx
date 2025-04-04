869b4921bf552f27cb1380b847b9849a9bb2ad54
unknown <vhvasudhan@gmail.com>
2025-04-04 19:07:35 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-[120vh] mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Vasudhan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />  
+
+      <p className="text-center text-white font-semibold text-[16px] sm:text-[20px] tracking-wide">
+        Click to Rotate
+      </p>
+
+    </section>
+  );
+};
+
+export default Hero;
